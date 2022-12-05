5659e5c06dc937b8bcb6e2b82b560a80bc6d3ef0
joaoricardoM <joao_pia_mds@hotmail.com>
2022-12-05 16:51:03 -0300

    update code

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,15 +1,29 @@
-const Article = ({ title, text, tags, image, alt}) => {
-    return <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center">
-        <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">
-            {title}
-        </h3>
-        <div className="grid gap-1">
-            {
-                text.map((content, index) => <p className="text-alura-200 dark:text-gray-400" key={index}>{content}</p>)
-            }
-        </div>
-        {image && <img className="p-4" src={image} />}
-        {image && alt && <span className="sr-only">{alt}</span>}
+const Article = ({ title, text, tags, image, alt }) => {
+  return (
+    <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center gap-2">
+      <h3 className="text-xl text-blue-200 dark:text-gray-200 font-bold">
+        {title}
+      </h3>
+      <div className="sm:w-full flex-row justify-end gap-2 pr-5 hidden sm:flex">
+        {tags.map((tag) => (
+          <span
+            key={tag}
+            className="bg-blue-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 font-bold text-xs uppercase"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+      <div className="grid gap-1">
+        {text.map((content, index) => (
+          <p className="text-blue-200 dark:text-gray-400" key={index}>
+            {content}
+          </p>
+        ))}
+      </div>
+      {image && <img className="p-4" src={image} />}
+      {image && alt && <span className="sr-only">{alt}</span>}
     </div>
-}
-export default Article
+  );
+};
+export default Article;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
-import ToggleTheme from "../toggleTheme/toggleTheme"
+import ToggleTheme from "../toggleTheme/toggleTheme";
 
-const Header = () => {
-    return <div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
-        <span className="text-gray-100">Olá, Usuario</span>
-        <h1 className="text-gray-100 text-xl">John's newsletter</h1>
-        <ToggleTheme />
+const Header = ({ user }) => {
+  return (
+    <div className="flex h-20 bg-blue-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+      <span className="text-gray-100">Olá, {user?.name || "Usuario"}</span>
+      <h1 className="text-gray-100 text-xl">John's newsletter</h1>
+      <ToggleTheme />
     </div>
-}
+  );
+};
 
-export default Header
+export default Header;
